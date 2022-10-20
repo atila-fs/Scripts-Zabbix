5b6bf9d1afc1cd7e7af351f8683b7be0efeab8d1
atila-fs <78006885+atila-fs@users.noreply.github.com>
2022-10-20 08:34:52 -0300

    Update VerificacaoOFFSET_DC.ps1

--- VerificacaoOFFSET_DC.ps1
+++ VerificacaoOFFSET_DC.ps1
@@ -1,2 +1,2 @@
-$s = w32tm /stripchart /computer:172.30.100.100 /samples:1 /dataonly
+$s = w32tm /stripchart /computer:IP do NTP /samples:1 /dataonly
 [regex]::match($s, '(\d{2}\.\d{7})').Groups[1].Value
